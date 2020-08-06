979652a0f1369506e3446ab8cd1ec2ec6f323dde
Spotky1004 <spotky1004@gmail.com>
2020-08-06 15:25:10 +0900

    57

--- script/script.js
+++ script/script.js
@@ -545,15 +545,15 @@ function loop(unadjusted, off = 0) {
   for (var i = 9; i > -1; i--) {
     multiThis = EN(1);
     if ((i == 0 || i == 9) && game.infUpgradeHave[0] == 1) {
-      multiThis = EN.mul(multiThis, 2);
+      multiThis = EN.mul(multiThis, 3);
     } else if ((i == 1 || i == 8) && game.infUpgradeHave[1] == 1) {
-      multiThis = EN.mul(multiThis, 2);
+      multiThis = EN.mul(multiThis, 3);
     } else if ((i == 2 || i == 7) && game.infUpgradeHave[2] == 1) {
-      multiThis = EN.mul(multiThis, 2);
+      multiThis = EN.mul(multiThis, 3);
     } else if ((i == 3 || i == 6) && game.infUpgradeHave[3] == 1) {
-      multiThis = EN.mul(multiThis, 2);
+      multiThis = EN.mul(multiThis, 3);
     } else if ((i == 4 || i == 5) && game.infUpgradeHave[4] == 1) {
-      multiThis = EN.mul(multiThis, 2);
+      multiThis = EN.mul(multiThis, 3);
     }
     if (i == 0 && game.infUpgradeHave[15] == 1) {
       multiThis = EN.mul(multiThis, Math.sqrt(game.infTime/30)*10+1);
