bc14958197165b6a019cb9b728c8886a52e5ae4b
Spotky1004 <spotky1004@gmail.com>
2020-09-15 19:55:48 +0900

    fix autobuyer interval diaplay (84)

--- script/script.js
+++ script/script.js
@@ -399,8 +399,7 @@ function randerAutobuyer() {
     }
     get('auto' + i + 'buy').innerHTML = '60% smaller interval<br>' + beautify(EN.mul(1e7, EN.pow(10, game.autobuyerBought[i]))) + ' OP'
     var thisAutoTick = autobuyerBaseTicks[i]*0.4**game.autobuyerBought[i];
-    var thisAutoTick = autobuyerBaseTicks[i]*0.4**game.autobuyerBought[i];
-    var autoBulk = Math.floor(1000/thisAutoTick)+1;
+    var autoBulk = Math.floor(1/thisAutoTick)+1;
     if (autoBulk > 100) {
       get('auto' + i + 'inter').innerHTML = 'Current interval: ' + beautify(autoBulk) + '/s';
     } else {
